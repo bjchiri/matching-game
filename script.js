c0a0e523407647cf43ae693d944934dbf7b7d16b
bjchiri05 <bjchiri05@gmail.com>
2022-11-29 21:21:56 -0700

    created DOM elements for matches and wins

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("javascript is connected")
+// DOM selectors
+
+let p1Wins = document.getElementById("p1wins");
+
+let p2Wins = document.getElementById("p2wins");
+
+let p1Matches = document.getElementById("p1matches");
+
+let p2Matches = document.getElementById("p2matches");
+
+
