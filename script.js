db0c342ef7c14213aa7985f9a8f9fe23709d6aa3
bjchiri05 <bjchiri05@gmail.com>
2022-12-01 22:51:00 -0700

    added reset button and verified functionality

--- script.js
+++ script.js
@@ -8,5 +8,10 @@ let p1Matches = document.getElementById("p1matches");
 
 let p2Matches = document.getElementById("p2matches");
 
+let reset = document.getElementById("reset");
+
+reset.addEventListener('click', () => {
+    console.log("you clicked the reset button");
+})
 
 p1Matches.innerHTML = 3;
